165571aee43258d9def5839611ce69292c209249
hminh <hminh250104@gmail.com>
2026-02-28 03:54:37 +0700

    refactor: add color scheme & heroui theme plugin

---
+++ heroui-plugin.js
@@ -0,0 +1,24 @@
+import { heroui } from "@heroui/theme/plugin";
+
+export default heroui({
+  themes: {
+    light: {
+      colors: {
+        primary: {
+          50: "#e6eaef",
+          100: "#b3bfcf",
+          200: "#8095af",
+          300: "#4d6b8f",
+          400: "#27426a",
+          500: "#011936",
+          600: "#01142b",
+          700: "#010f20",
+          800: "#000a16",
+          900: "#00050b",
+          DEFAULT: "#011936",
+          foreground: "#ffffff",
+        },
+      },
+    },
+  },
+});
